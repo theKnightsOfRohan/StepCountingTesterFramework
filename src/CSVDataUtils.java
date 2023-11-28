cd73b76b662eb240e6f167a2d58e3a1ed18ca543
theKnightsOfRohan <shadowsovereign100@gmail.com>
2023-11-28 12:12:09 -0800

    Config

---
+++ src/CSVDataUtils.java
@@ -0,0 +1,87 @@
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class CSVDataUtils {
+	public static HashMap<String, List<Double>> parseCSVString(HashMap<String, List<String>> dataAsStrings) {
+		HashMap<String, List<Double>> data = new HashMap<String, List<Double>>();
+		for (String key : dataAsStrings.keySet()) {
+			data.put(key, new ArrayList<Double>());
+			for (String value : dataAsStrings.get(key)) {
+				data.get(key).add(Double.parseDouble(value));
+			}
+		}
+		return data;
+	}
+
+	public static HashMap<String, List<String>> readCSVFileAsMap(String filePath) {
+		HashMap<String, List<String>> data = new HashMap<String, List<String>>();
+		try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+			String[] lineNames = reader.readLine().split(",");
+			for (String name : lineNames) {
+				data.put(name, new ArrayList<String>());
+			}
+			String line = reader.readLine();
+			while (line != null) {
+				String[] row = line.split(",");
+				for (int i = 0; i < row.length; i++) {
+					data.get(lineNames[i]).add(row[i]);
+				}
+				line = reader.readLine();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return data;
+	}
+
+	public static List<Double> getMagnitudes(List<Double> x, List<Double> y, List<Double> z) {
+		List<Double> magnitudes = new ArrayList<Double>();
+		for (int i = 0; i < x.size(); i++) {
+			magnitudes.add(Math.sqrt(Math.pow(x.get(i), 2) + Math.pow(y.get(i), 2) + Math.pow(z.get(i), 2)));
+		}
+		return magnitudes;
+	}
+
+	public static void writeToCSVFile(String filePath, List<Double> data) {
+		try (java.io.PrintWriter writer = new java.io.PrintWriter(filePath)) {
+			for (int i = 0; i < data.size(); i++) {
+				writer.println(i + "," + data.get(i));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static List<Double> applyBasicMedianFilter(List<Double> magnitudes) {
+		Arrays.sort(magnitudes.toArray());
+		return magnitudes.subList(magnitudes.size() / 2, magnitudes.size());
+	}
+
+	public static List<Double> applyMovingAverage(List<Double> magnitudes, int spread) {
+		List<Double> filteredMagnitudes = new ArrayList<Double>();
+		for (int i = 0; i < magnitudes.size(); i++) {
+			double sum = 0;
+			for (int j = i - spread; j < i + spread; j++) {
+				if (j >= 0 && j < magnitudes.size()) {
+					sum += magnitudes.get(j);
+				}
+			}
+			filteredMagnitudes.add(sum / (spread * 2 + 1));
+		}
+		return filteredMagnitudes;
+	}
+
+	public static String getOutputPath(String filePath) {
+		List<String> pathParts = Arrays.asList(filePath.split("/"));
+		pathParts.set(pathParts.size() - 2, "Output");
+		String fileName = pathParts.get(pathParts.size() - 1);
+		pathParts.set(pathParts.size() - 1, fileName.substring(0, fileName.length() - 4) + "_output.txt");
+		return String.join("/", pathParts);
+	}
+}
---
+++ src/ReadStepsData.java
@@ -0,0 +1,98 @@
+import Utils.FileIO;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Arrays;
+import org.apache.commons.math3.fitting.HarmonicCurveFitter;
+import org.apache.commons.math3.fitting.WeightedObservedPoints;
+import Plot.PlotWindow;
+import Plot.ScatterPlot;
+
+public class ReadStepsData {
+	public static void main(String[] args) {
+		processData("src/ProblemSets/W12/Steps_Data/1-200-step-regular.csv", 0);
+		processData("src/ProblemSets/W12/Steps_Data/2-200-step-variable.csv", 0);
+		processData("src/ProblemSets/W12/Steps_Data/3-200-step-jacket.csv", 0);
+	}
+
+	public static void processData(String filePath, double threshold) {
+		String ouputPath = CSVDataUtils.getOutputPath(filePath);
+		String outputStr = "Threshold: " + threshold + "\n";
+
+		HashMap<String, List<String>> dataAsStrings = CSVDataUtils.readCSVFileAsMap(filePath);
+
+		HashMap<String, List<Double>> data = CSVDataUtils.parseCSVString(dataAsStrings);
+
+		List<Double> magnitudes = CSVDataUtils.getMagnitudes(data.get("BMI160_accelerometer.x"),
+				data.get("BMI160_accelerometer.y"),
+				data.get("BMI160_accelerometer.z"));
+
+		outputStr += "Length of data before filtering: " + magnitudes.size() + "\n";
+
+		// magnitudes = CSVDataUtils.applyBasicMedianFilter(magnitudes);
+		// outputStr += "Length of data after median filter: " + magnitudes.size() +
+		// "\n";
+
+		magnitudes = CSVDataUtils.applyMovingAverage(magnitudes, 5);
+		outputStr += "Number of steps after moving average: " + calculateSteps(magnitudes, threshold) + "\n";
+		FileIO.writeToFile(ouputPath, outputStr);
+
+		plotData(magnitudes, threshold);
+	}
+
+	private static int calculateSteps(List<Double> magnitudes, double threshold) {
+		int stepCount = 0;
+		for (int i = 1; i < magnitudes.size() - 1; i++) {
+			if (isPeak(magnitudes, i, threshold) || isValley(magnitudes, i, threshold)) {
+				stepCount++;
+			}
+		}
+
+		return stepCount;
+	}
+
+	private static void plotData(List<Double> data, double threshold) {
+		ScatterPlot plt = new ScatterPlot(100, 100, 1100, 700);
+		WeightedObservedPoints obs = new WeightedObservedPoints();
+
+		for (int i = 0; i < data.size(); i++) {
+			plt.plot(0, i, data.get(i)).strokeColor("red").strokeWeight(2).style("-");
+			obs.add(i, data.get(i));
+
+			if (isPeak(data, i, threshold) || isValley(data, i, threshold)) {
+				plt.plot(1, i, data.get(i)).strokeColor("green").strokeWeight(2).style(".");
+			}
+		}
+
+		HarmonicCurveFitter fitter = HarmonicCurveFitter.create();
+		double[] params = fitter.fit(obs.toList());
+		// Print the equation for the fitted curve
+		System.out.println(params[0] + " * sin(" + params[1] + " * x + " + params[2] + ")");
+
+		for (int i = 0; i < data.size(); i++) {
+			plt.plot(2, i, params[0] * Math.sin(params[1] * i + params[2]) + 10).strokeColor("blue").strokeWeight(2)
+					.style("-");
+		}
+
+		PlotWindow window = PlotWindow.getWindowFor(plt, 1200, 800);
+		window.show();
+	}
+
+	private static boolean isPeak(List<Double> magnitudes, int index, double threshold) {
+		try {
+			return magnitudes.get(index) - magnitudes.get(index - 1) > threshold
+					&& magnitudes.get(index) - magnitudes.get(index + 1) > threshold;
+		} catch (IndexOutOfBoundsException e) {
+			return false;
+		}
+	}
+
+	private static boolean isValley(List<Double> magnitudes, int index, double threshold) {
+		try {
+			return magnitudes.get(index - 1) - magnitudes.get(index) > threshold
+					&& magnitudes.get(index + 1) - magnitudes.get(index) > threshold;
+		} catch (IndexOutOfBoundsException e) {
+			return false;
+		}
+	}
+
+}
